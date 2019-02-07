d0838d57b2429bc36b7e79df014d5a176b1fd856
Judy Tuan <judytuna@gmail.com>
2019-02-06 19:23:54 -0800

    Do as little as possible to make the single failing test pass, even though I know other functionality will break.

--- app/Calculator.js
+++ app/Calculator.js
@@ -3,8 +3,10 @@ function Calculator() {
 Calculator.prototype.add = function(numbers) {
   this.answer = 0;
 
-  if(numbers[0]) {
-    this.answer = parseInt(numbers[0]);
+  let numbersAsArray = numbers.split(",");
+
+  if(numbersAsArray[0]) {
+    this.answer += parseInt(numbersAsArray[0]);
   }
 
   if(numbers[2]) {
