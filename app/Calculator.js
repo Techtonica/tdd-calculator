b8622b5c42c7ba577c8fa96ff2f23316532b3271
Judy Tuan <judytuna@gmail.com>
2019-02-06 18:39:28 -0800

    Code demo. Make 2 tests pass: 0 arguments and 1 argument.

--- app/Calculator.js
+++ app/Calculator.js
@@ -3,7 +3,11 @@ function Calculator() {
 Calculator.prototype.add = function(numbers) {
   this.answer = 0;
 
-  return this.answer; // in the beginning, do the simplest possible thing to make one test pass
+  if(numbers[0]) {
+    this.answer = parseInt(numbers[0]);
+  }
+
+  return this.answer;
 }
 
 module.exports = Calculator;
